bb3b86f8c6393e6c5ad049763dafa6709dd3703d
Nahum Alon <nochum.shepsl@gmail.com>
2024-08-18 15:59:42 +0300

    Added helping method assertGivenAndShuffle in tests

--- src/main/java/telran/stream/StreamTasks.java
+++ src/main/java/telran/stream/StreamTasks.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class StreamTasks {
-    public static int[] suffle(int[] arr) {
+    public static int[] shuffle(int[] arr) {
         return Arrays.stream(arr).boxed().sorted((a, b) -> new Random().nextInt(2) - 1).mapToInt(n -> n).toArray();
     }
 }
--- src/test/java/telran/stream/StreamTaskTest.java
+++ src/test/java/telran/stream/StreamTaskTest.java
@@ -6,12 +6,22 @@ import org.junit.jupiter.api.Test;
 
 public class StreamTaskTest {
     @Test
-    void suffleTest() {
-       int[] arrayGiven = { 2, 8, -7, 12, 0, 99 };
-       int[] arraySuffled = new int[arrayGiven.length];
-       arraySuffled = StreamTasks.suffle(arrayGiven);
-       java.util.Arrays.sort(arrayGiven);
-       java.util.Arrays.sort(arraySuffled);
-       assertArrayEquals(arrayGiven, arraySuffled);
+    void shuffleTest() {
+        int[] arr1 = { 2, 8, -7, 12, 0, 99 };
+        assertGivenAndShuffle(arr1);
+        int[] arr2 = { 2 };
+        assertGivenAndShuffle(arr2);
+        int[] arr3 = {};
+        assertGivenAndShuffle(arr3);
+        int[] arr4 = {1, 2, 3, 4, 5, 6};
+        assertGivenAndShuffle(arr4);
+    }
+
+    void assertGivenAndShuffle(int[] arrayGiven) {
+        int[] arraySuffled = new int[arrayGiven.length];
+        arraySuffled = StreamTasks.shuffle(arrayGiven);
+        java.util.Arrays.sort(arrayGiven);
+        java.util.Arrays.sort(arraySuffled);
+        assertArrayEquals(arrayGiven, arraySuffled);
     }
 }
