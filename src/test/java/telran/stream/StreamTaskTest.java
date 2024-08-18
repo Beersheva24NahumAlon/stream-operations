ae7904d131852e55b53830de8753321a345d02a8
Nahum Alon <nochum.shepsl@gmail.com>
2024-08-18 16:30:21 +0300

    Made some cosmetic fixes

--- src/test/java/telran/stream/StreamTaskTest.java
+++ src/test/java/telran/stream/StreamTaskTest.java
@@ -1,6 +1,5 @@
 package telran.stream;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
